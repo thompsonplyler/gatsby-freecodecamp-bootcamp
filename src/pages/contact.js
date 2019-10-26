3675e749ec33473eddec6aaa4b75368246549142
Thompson Plyler <nealok.g@gmail.com>
2019-10-26 13:04:26 -0400

    First commit. Continuing to learn Gatsby

---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Contact = () => {
+    return(
+        <div>
+            <h1>This is a contact page!</h1>
+            <p>The best way to contact me is a message <a href="http://twitter.com/thompsonplyler" target="_blank" >@thompsonplyler on Twitter</a></p>
+        </div>
+    )
+}
+
+export default Contact
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,21 @@
-import React from "react"
+import React from 'react'
+import {Link} from 'gatsby'
 
-export default () => <div>Hello world!</div>
+const Index = () => {
+    return (
+        <div>
+            <h1>Hello!</h1>
+            <h2>My name is Thompson. I'm a full-stack developer living in NYC.</h2>
+            <p>Do you need a website developer? <Link to="/contact">Contact me here.</Link></p>
+        </div>
+    )
+}
+
+export default Index
+
+/*
+ // Initial page setup:
+ // ---------------------------
+ import React from "react"
+ export default () => <div>Hello world!</div>
+*/
